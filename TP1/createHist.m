bd83c30551a5b5f3a70f453c1df7303ac015cc4f
David Gomes <davidrafagomes@gmail.com>
2014-10-04 19:55:57 +0100

    Code style fixes.

--- TP1/createHist.m
+++ TP1/createHist.m
@@ -1,11 +1,13 @@
 function count = createHist(P, A)
+    % Mostra e devolve o histograma de ocorr?ncia dos s?mbolos
+    % do alfabeto A na fonte de informa??o P
+
     count = zeros(size(A));
-    
+
     for i=1:length(A)
        f = length(find(P == A(i)));
        count(i) = f;
     end
-    
-	waitfor(bar(count));
 
+    waitfor(bar(count));
 end
--- TP1/ex3.m
+++ TP1/ex3.m
@@ -12,7 +12,7 @@ createHist(im, [0 255]);
 entropia(im, [0 255])
 % 3
 wav = wavread('dados/guitarSolo.wav');
-quant = 8
+quant = 8;
 d = 1 / (2^8);
 createHist(wav, -1:1:d);
 entropia(wav, -1:1:d)
