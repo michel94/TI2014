0adf5b0f4c2ab18b9420fdfe0aba3c79ce9c381e
David Gomes <davidrafagomes@gmail.com>
2014-10-07 09:32:12 +0100

    Replaces wavread with audioread.

--- TP1/ex4.m
+++ TP1/ex4.m
@@ -11,7 +11,7 @@ im = imread('dados/homerBin.bmp');
 c = hist(im, [0 255]);
 hufflen(c)
 % 4
-wav = wavread('dados/guitarSolo.wav');
+wav = audioread('dados/guitarSolo.wav');
 quant = 7;
 d = 1 / (2^quant);
 alf = -1:d:1;
