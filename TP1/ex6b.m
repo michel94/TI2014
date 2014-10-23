7a369f17435876ac70450a66bc0ca93e2755cf3c
michel94 <miguel.duarte20@hotmail.com>
2014-10-23 13:42:44 +0100

    Finished ex6. Untested.

--- TP1/ex6b.m
+++ TP1/ex6b.m
@@ -5,7 +5,13 @@ quant = 7;
 d = 1 / (2^quant);
 alf = -1:d:1;
 
+
 wav = audioread('dados/target01 - repeat.wav');
 wav = wav(:);
-
 r = slidingWindow(query, wav, alf, floor(0.25*length(query)) )
+plot(r);
+
+wav = audioread('dados/target02 - repeatNoise.wav');
+wav = wav(:);
+r = slidingWindow(query, wav, alf, floor(0.25*length(query)) )
+plot(r);
